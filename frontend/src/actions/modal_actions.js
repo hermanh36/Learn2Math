dc9e5e6aef2defe2a24738c8d370f91b3fa33367
rmei6 <appacademystudent@Apps-Mac-mini-2.local>
2022-05-09 16:34:03 -0400

    modal

---
+++ frontend/src/actions/modal_actions.js
@@ -0,0 +1,11 @@
+export const OPEN_MODAL = "OPEN_MODAL";
+export const CLOSE_MODAL = "CLOSE_MODAL";
+
+const openModal = (modal) => ({
+  type: OPEN_MODAL,
+  modal
+});
+
+// const closeModal = () => ({
+
+// })
