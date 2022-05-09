7589fe6df77caf8cf35d705d09f4f05c36a90618
App Academy Student <email@email.com>
2022-05-09 18:18:02 -0400

    lesson routes update

---
+++ frontend/src/util/lesson_util.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+export const fetchLessons = () => {
+  return axios.get('/api/lessons')
+};
+
+export const fetchLesson = lessonId => {
+  return axios.get(`/api/lessons/${lessonId}`)
+};
+
+export const createLesson = lesson => {
+  return axios.post('api/lessons/', lesson)
+};
+
+export const updateLesson = lesson => {
+  return axios.patch(`api/lessons/${lesson.id}`, lesson)
+};
+
+export const deleteLesson = lessonId => {
+  return axios.delete(`api/lessons/${lessonId}`)
+}
