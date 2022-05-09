826048a332a5dff15114981ce0a4bacfb655b846
Derek <fataldigitz@gmail.com>
2022-05-09 11:07:25 -0400

    finish flashcard and question model

---
+++ models/Flashcard.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const FlashcardScehma = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: Text,
+        required: true
+    },
+    authorId: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    }}, 
+    {timestamps: true
+})
+
+module.exports = Flashcard = mongoose.model('Flashcard', FlashcardScehma);
---
+++ models/Question.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const QuestionSchema = new Schema({
+    quiz: {
+        type: Schema.Types.ObjectId,
+        ref: 'quiz'
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: Array,
+        required: true
+    },
+    answerChoices: {
+        type: Array,
+        required: true
+    },
+    correctAnswers: {
+        type: Array,
+        required: true
+    }
+})
+
+module.exports = Question = mongoose.model('Question', QuestionSchema);
