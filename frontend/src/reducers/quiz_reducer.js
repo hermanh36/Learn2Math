9daeb3fd0244e13d8fc3df083be0f5ac66499c82
Derek <fataldigitz@gmail.com>
2022-05-10 16:39:36 -0400

    quiz reducer

---
+++ frontend/src/reducers/quiz_reducer.js
@@ -0,0 +1,29 @@
+import {
+    RECEIVE_QUIZZES,
+    RECEIVE_QUIZ,
+    REMOVE_QUIZ
+} from '../actions/quiz_action';
+
+const QuizReducer = (state={}, action) => {
+    Object.freeze(state);
+    const nextState = Object.assign({}, state);
+
+    switch(action.type){
+        case RECEIVE_QUIZZES:
+            Object.values(action.quizzes).forEach(quiz => {
+                nextState[quiz._id] = quiz
+            })
+            return nextState;
+        case RECEIVE_QUIZ:
+            nextState[action.quiz._id] = action.quiz;
+            return nextState;
+        case REMOVE_QUIZ:
+            delete nextState[action.quizId];
+            return nextState;
+        default:
+            return state;
+            
+    }
+}
+
+export default QuizReducer;
