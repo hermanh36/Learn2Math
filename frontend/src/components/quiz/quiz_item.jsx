bb3e35665192019cb0b4a227189dc8b38ca7ce31
App Academy Student <email@email.com>
2022-05-11 12:14:20 -0400

    quiz answers stuff

--- frontend/src/components/quiz/quiz_item.jsx
+++ frontend/src/components/quiz/quiz_item.jsx
@@ -5,11 +5,32 @@ class QuizItem extends React.Component {
   constructor(props){
     super(props);
     this.state = { questions : [] };
+    this.submitHandler = this.submitHandler.bind(this);
   }
   //assume props have lessonId
 
   componentDidMount() {
     this.props.fetchQuestions(this.props.quizId)
+  }
+
+  submitHandler() {
+    let correctAnswer = [];
+    let submittedAnswer = [];
+    let score = 0;
+    const total = this.props.questions.length;
+    this.props.questions.forEach(question => {
+      correctAnswer.push(question.correctAnswer);
+      let currentInput = document.querySelector(`input[name='answer${question._id}']:checked`) 
+      ? document.querySelector(`input[name='answer${question._id}']:checked`).value
+      : 'empty';
+      submittedAnswer.push(currentInput);
+    });
+    correctAnswer.forEach((answer,index) => {
+      if (answer === submittedAnswer[index]){
+        score += 1;
+      }
+    })
+    console.log(score);
   }
 
   render() {
@@ -24,7 +45,7 @@ class QuizItem extends React.Component {
               {this.props.questions.map(question => <QuestionItemContainer question={question}/>)}
             </ul>
           </div>
-          
+          <button onClick={this.submitHandler}>Submit</button>
         </div>
       )
     }
