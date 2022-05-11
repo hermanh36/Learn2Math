8a88a4741ffcf862096c1935360e7dbcb88f7cc4
ds <swaby@protonmail.com>
2022-05-10 22:18:04 -0400

    add search functionality to category index

--- frontend/src/components/category/category_index.jsx
+++ frontend/src/components/category/category_index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import LeftSidebar from '../left_sidebar/left_sidebar';
 import BorderBtn from '../border_btn/border_btn';
+import { BiSearchAlt } from 'react-icons/bi';
 var parser = new DOMParser();
 
 export default class CategoryIndex extends React.Component {
+
+    constructor(props) {
+        super(props); 
+        this.state = {
+            searchText: ""
+        }
+    }
 
     componentDidMount(){
         this.props.fetchLessons().then((res) => {
@@ -18,19 +26,25 @@ export default class CategoryIndex extends React.Component {
         return dom;
     };
 
+    onChangeSearch(e){
+        this.setState({
+            searchText: e.currentTarget.value 
+        })
+    }
+
     render() {
         
         // let dummy = parser.parseFromString("<p>Naran is cool</p>" ,"text/html"); 
         
         let algebraLessons = this.props.lessons.map((lesson) => {
-            if(lesson.category === 'algebra'){
+            if(lesson.category === 'algebra' && lesson.title.includes(this.state.searchText)){
                 return (
                   <BorderBtn lesson={lesson} />
                 )
             }
         })
         let geoLessons = this.props.lessons.map((lesson) => {
-            if(lesson.category === 'geometry'){
+            if(lesson.category === 'geometry' && lesson.title.includes(this.state.searchText)){
                 return (
                   <BorderBtn lesson={lesson} />
                 )
@@ -41,6 +55,10 @@ export default class CategoryIndex extends React.Component {
             <div className="category-index-wrap">
                 <LeftSidebar />
                 <div>
+                    <p className="search-input-wrap">
+                        <input type="text" placeholder="search lessons" value={this.state.searchText} onChange={(e) => this.onChangeSearch(e)}/>
+                        <BiSearchAlt className="search-icon" />
+                    </p>
                     <div>
                         <div className="category-wrap">
                             <h2>Algebra</h2>
