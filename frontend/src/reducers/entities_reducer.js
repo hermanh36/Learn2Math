2d3f5d857165880c6e21dddbb84004f1b6ef5f81
Derek <fataldigitz@gmail.com>
2022-05-11 16:01:54 -0400

    flashcards

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -2,12 +2,13 @@ import {combineReducers} from 'redux';
 import LessonsReducer from './lessons_reducer';
 import QuestionReducer from './question_reducer';
 import QuizReducer from './quiz_reducer';
+import FlashcardReducer from './flashcard_reducer';
 
 const EntitiesReducer = combineReducers({
     questions: QuestionReducer,
     lessons: LessonsReducer,
     quizzes: QuizReducer,
-    flashcards:
+    flashcards: FlashcardReducer
 })
 
 export default EntitiesReducer;
---
+++ frontend/src/reducers/flashcard_reducer.js
@@ -0,0 +1,11 @@
+import {
+    RECEIVE_FLASHCARD,
+    RECEIVE_FLASHCARDS,
+    REMOVE_FLASHCARD
+} from '../actions/flashcard_actions';
+
+const FlashcardReducer = (state={}, action) => {
+
+}
+
+export default FlashcardReducer;
