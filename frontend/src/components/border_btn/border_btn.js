1bbe1f61503bcf75cfa7b39d89e659bf4fcee1ac
Derek <fataldigitz@gmail.com>
2022-05-11 21:48:46 -0400

    fix merge conflict

--- frontend/src/components/border_btn/border_btn.js
+++ frontend/src/components/border_btn/border_btn.js
@@ -4,11 +4,7 @@ import { Link } from "react-router-dom";
 const BorderBtn = (props) => {
     return (
          <li>
-<<<<<<< HEAD
             <Link to={`/lesson/${props.lesson._id}`}>
-=======
-            <Link to={``}>
->>>>>>> 0bbdfc1b4485413426796239feba4dea6e658b94
                 <span></span>
                 <span></span>
                 <span></span>
