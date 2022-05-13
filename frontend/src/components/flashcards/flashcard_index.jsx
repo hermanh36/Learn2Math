d36047429d0152bf471278ecdf7100b38928faa8
Derek <fataldigitz@gmail.com>
2022-05-12 22:23:48 -0400

    flashcard index

--- frontend/src/components/flashcards/flashcard_index.jsx
+++ frontend/src/components/flashcards/flashcard_index.jsx
@@ -1,27 +1,43 @@
 import React from "react";
 
-export default class FlashcardIndex extends React.Component{
-    constructor(props){
+export default class FlashcardIndex extends React.Component {
+    constructor(props) {
         super(props);
-        this.state=this.props.flashcards;
+        this.state = this.props.flashcards;
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.fetchFlashcards(this.props.match.params.userId).then(res => console.log(res));
     }
 
-    componentWillReceiveProps(nextProps){
-        this.setState({flashcards: nextProps.flashcards})
+    componentWillReceiveProps(nextProps) {
+        this.setState({ flashcards: nextProps.flashcards })
     }
 
-    render(){
+    render() {
         debugger;
+        let flashcards;
         if (this.state.flashcards) {
             console.log(Object.values(this.state.flashcards));
+            flashcards = Object.values(this.state.flashcards).map((flashcard, idx) => {
+                return (
+                    <div key={flashcard._id} id={`flashcard-${idx}`}> {/*className= flashcard container */}
+                        <div> {/* word*/}
+                            {flashcard.title}
+                        </div>
+                        <div> {/* definition, give this a class of hidden initially */}
+                            {flashcard.body}
+                        </div>
+                        <br /> {/* can remove line break when starting to style */}
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </div>
+                )
+            })
         }
         return (
             <>
-                inside flashcard index
+                {flashcards}
             </>
         )
     }
