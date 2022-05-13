e8f984ded00685612cac2c49faeef7c54f1853b0
Derek <fataldigitz@gmail.com>
2022-05-13 00:15:22 -0400

    lesson_show page styling

--- frontend/src/components/lessons/lesson_show.jsx
+++ frontend/src/components/lessons/lesson_show.jsx
@@ -1,84 +1,93 @@
+//lesson_show.jsx
 import React from "react";
 import LeftSidebar from "../left_sidebar/left_sidebar_container";
 import parse from 'html-react-parser';
 import { Link } from "react-router-dom";
 
 class LessonShow extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       questions: this.props.questions,
-      quizzes: this.props.quizzes
+      quizzes: this.props.quizzes,
+      users: this.props.users
     }
   }
 
-  componentDidMount() { 
-    const {fetchLesson, fetchQuiz, fetchQuestions, lessonId, fetchUsers} = this.props;
+  componentDidMount() {
+    const { fetchLesson, fetchQuiz, fetchQuestions, lessonId, fetchUsers } = this.props;
     fetchLesson(lessonId)
       .then(lesson => fetchQuiz(lesson.lesson._id))
       .then(quiz => fetchQuestions(quiz.quiz._id))
       .then(() => fetchUsers())
+      .then(() => {
+        console.log(this.state);
+        // debugger;
+      })
   }
 
-  componentWillReceiveProps(nextProps){
+  componentWillReceiveProps(nextProps) {
     // debugger;
-    this.setState({quizzes: nextProps.quizzes, questions: nextProps.questions, users: nextProps.users})
+    this.setState({ quizzes: nextProps.quizzes, questions: nextProps.questions, users: nextProps.users })
     // console.log(nextProps.questions)
     // console.log(nextProps.quizzes)
   }
 
-  renderLessonContent(){
+  renderLessonContent() {
 
   }
-//{this.props.users[this.props.lesson.authorId].email}
-  render(){
+  //{this.props.users[this.props.lesson.authorId].email}
+  render() {
     // console.log(this.state.questions.length);
     console.log(this.props.currentUserId);
-    console.log(this.state.users);
+
+    // console.log(this.state.users);
     let currentUserEmail;
-    if (this.state.users) console.log(Object.values(this.state.users).forEach(user =>{if (user._id) currentUserEmail = user.email}));
+    if (this.state.users) Object.values(this.state.users).forEach(user => { if (user._id === this.props.lesson.authorId) currentUserEmail = user.email });
+    console.log(currentUserEmail);
     if (!this.props.lesson) {
       return null
-    } 
+    }
     else {
       let quizId, questionsLength;
       if (this.state.quizzes) quizId = Object.keys(this.state.quizzes)[0];
       if (this.state.questions) questionsLength = Object.keys(this.state.questions).length;
-      const {currentUserId} = this.props;
+      const { currentUserId } = this.props;
       const authorId = this.props.lesson.authorId;
       const takeQuiz = this.state.questions.length > 0 ? <Link className="lesson-quiz-redirect-button" to={`/quiz/${quizId}`}>Take Quiz</Link> : <></>;
-    
-    return (
-      <div className="lesson-show-wrap">
-        <LeftSidebar />
-          
-        {this.props.lesson ? 
-        
-          (
-            <div className="lesson-show-container ql-editor">
 
-              <div className="lesson-show-title">{this.props.lesson.title} by {currentUserEmail} </div>
-          
-              <div id="lesson-html-content">{parse(this.props.lesson.content)}</div>
+      // debugger;
+      return (
+        <div className="lesson-show-wrap">
+          <LeftSidebar />
 
-              {currentUserId === authorId ? ( 
-              <>
-                  <Link to={`/lesson/${this.props.match.params.lessonId}/edit`}><button>Edit Lesson</button></Link>
-                  <div className="lesson-quiz-redirect-wrap">
-                    <Link className="lesson-quiz-redirect-button" to={`/quiz/${quizId}/edit`}>Edit Quiz</Link>
-                  </div>
-              </>)
-                : 
-                // {takeQuiz}
-                <button>Take Quiz Button</button>
+          {this.props.lesson ?
+
+            (
+              <div className="lesson-show-container ql-editor">
+
+                <div className="lesson-show-title">{this.props.lesson.title} <span className='author' >by {currentUserEmail}</span> </div>
+
+                <div id="lesson-html-content">{parse(this.props.lesson.content)}</div>
+
+                {currentUserId === authorId ? (
+                  <>
+                    <Link to={`/lesson/${this.props.match.params.lessonId}/edit`}><button>Edit Lesson</button></Link>
+                    <div className="lesson-quiz-redirect-wrap">
+                      <Link className="lesson-quiz-redirect-button" to={`/quiz/${quizId}/edit`}>Edit Quiz</Link>
+                    </div>
+                  </>)
+                  :
+                  // {takeQuiz}
+                  <button>Take Quiz Button</button>
                 }
-            </div> 
-          )
-          : null
-        }
-      </div>
-  
-    )
+              </div>
+            )
+            : null
+          }
+        </div>
+
+      )
 
 
 
--- frontend/src/components/lessons/lesson_show_container.js
+++ frontend/src/components/lessons/lesson_show_container.js
@@ -1,12 +1,13 @@
+//lesson_show_container.js
 import { connect } from "react-redux";
 import LessonShow from "./lesson_show";
 import { fetchLesson } from "../../actions/lesson_actions";
-import {fetchQuiz} from '../../actions/quiz_action';
-import {fetchQuestions} from '../../actions/question_actions';
+import { fetchQuiz } from '../../actions/quiz_action';
+import { fetchQuestions } from '../../actions/question_actions';
 import { fetchUsers } from "../../actions/user_actions";
 
 const mSTP = (state, ownProps) => {
-  
+  debugger;
   return {
     lesson: state.entities.lessons[ownProps.match.params.lessonId],
     lessonId: ownProps.match.params.lessonId,
@@ -18,10 +19,10 @@ const mSTP = (state, ownProps) => {
 };
 
 const mDTP = (dispatch) => ({
-    fetchLesson: (lessonId) => dispatch(fetchLesson(lessonId)),
-    fetchQuiz: lessonId => dispatch(fetchQuiz(lessonId)),
-    fetchQuestions: quizId => dispatch(fetchQuestions(quizId)),
-    fetchUsers: () => dispatch(fetchUsers())
+  fetchLesson: (lessonId) => dispatch(fetchLesson(lessonId)),
+  fetchQuiz: lessonId => dispatch(fetchQuiz(lessonId)),
+  fetchQuestions: quizId => dispatch(fetchQuestions(quizId)),
+  fetchUsers: () => dispatch(fetchUsers())
 })
 
 export default connect(mSTP, mDTP)(LessonShow);
