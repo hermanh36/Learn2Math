a5e6397d435866c249183c50804951ff6aa4465a
Herman <hjiper2@DESKTOP-EU3DC6S.localdomain>
2022-05-12 20:06:23 -0400

    profile comment out

--- frontend/src/components/profile/profile_container.js
+++ frontend/src/components/profile/profile_container.js
@@ -1,23 +1,23 @@
-// src/components/profile/profile_container.js
+// // src/components/profile/profile_container.js
 
-import { connect } from 'react-redux';
-import Profile from './profile';
-import { fetchMyLessons } from '../../actions/lesson_actions';
+// import { connect } from 'react-redux';
+// import Profile from './profile';
+// import { fetchMyLessons } from '../../actions/lesson_actions';
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    lessons: state.entities.lessons,
-    lesson: state.entities.lessons[ownProps.match.params.lessonId],
-    user: state.entities.user[ownProps.match.params.userId]
-  };
-};
+// const mapStateToProps = (state, ownProps) => {
+//   return {
+//     lessons: state.entities.lessons,
+//     lesson: state.entities.lessons[ownProps.match.params.lessonId],
+//     user: state.entities.user[ownProps.match.params.userId]
+//   };
+// };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    fetchMyLessons: authorId => dispatch(fetchMyLessons(authorId)),
-    fetchQuizScore: quizScore => dispatch(fetchQuizScore(quizScore)),
-    fetchLesson: lesson => dispatch(fetchLesson(lesson))
-  };
-};
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     fetchMyLessons: authorId => dispatch(fetchMyLessons(authorId)),
+//     fetchQuizScore: quizScore => dispatch(fetchQuizScore(quizScore)),
+//     fetchLesson: lesson => dispatch(fetchLesson(lesson))
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Profile);
+// export default connect(mapStateToProps, mapDispatchToProps)(Profile);
