045645c7497c11c9bd9caa24a8d3801c1c2516ac
Herman <hjiper2@DESKTOP-EU3DC6S.localdomain>
2022-05-25 12:58:53 -0400

    lesson show page render fix

--- frontend/src/components/lessons/lesson_show.jsx
+++ frontend/src/components/lessons/lesson_show.jsx
@@ -9,12 +9,6 @@ import CreateCommentContainer from "../comment/create_comment_container";
 class LessonShow extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      questions: this.props.questions,
-      quizzes: this.props.quizzes,
-      users: this.props.users,
-      comments: this.props.comments
-    }
     this.deleteHandler = this.deleteHandler.bind(this);
   }
 
@@ -31,11 +25,6 @@ class LessonShow extends React.Component {
       // .then(() => fetchUsers())
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({ quizzes: nextProps.quizzes, questions: nextProps.questions, comments: nextProps.comments, users: nextProps.users})
-    // console.log(nextProps.questions)
-    // console.log(nextProps.quizzes)
-  }
 
   deleteComment(commentId){
     return e => {
@@ -74,15 +63,13 @@ class LessonShow extends React.Component {
 
 
   render() {
-    // console.log(this.state.questions.length);
-    console.log(this.props.currentUserId);
-    console.log(this.state);
-    const {users, comments} = this.state;
-    // console.log(this.state.users);
-    const commentsForThisLesson = (Object.values(comments).length > 0) && (Object.values(users).length>0) ? (
+    const {users, comments, lesson, lessonId, quizzes, questions, currentUserId} = this.props;
+    const { clearUsers, fetchUser, fetchCommentsByLesson, fetchLesson, fetchQuizByLessonId, fetchQuestions, fetchUsers } = this.props;
+    console.log(users[comments.userId])
+    const commentsForThisLesson = (Object.values(comments).length > 0) && (Object.values(users).length > 1) ? (
       <div className="comment-section-wrap">
         <h1>Comments:</h1>
-        {Object.values(this.state.comments).map(comment =>(
+        {Object.values(comments).map(comment =>(
         <div >
           <p className="comment-author">{this.trimEmail(users[comment.userId].email)}</p>
           <div className="comment-wrap">
@@ -104,9 +91,6 @@ class LessonShow extends React.Component {
         </div>))}
       </div>
     ) : null;
-    // let currentUserEmail;
-    // if (this.state.users) Object.values(this.state.users).forEach(user => { if (user._id === this.props.lesson.authorId) currentUserEmail = user.email });
-    // console.log(currentUserEmail);
     console.log(this.props.users);
     if (!this.props.lesson) {
       return null
@@ -114,13 +98,10 @@ class LessonShow extends React.Component {
     else {
       //FIX QUIZ ID
       let quizId, questionsLength;
-      if (this.state.quizzes) quizId = Object.keys(this.state.quizzes)[0];
-      if (this.state.questions) questionsLength = Object.keys(this.state.questions).length;
+      if (quizzes) quizId = Object.keys(quizzes)[0];
       const { currentUserId } = this.props;
       const authorId = this.props.lesson.authorId;
-      const takeQuiz = this.state.questions.length > 0 ? <Link className="lesson-quiz-redirect-button" to={`/quiz/${quizId}`}>Take Quiz</Link> : <></>;
-
-      if (this.props.users && Object.values(this.props.users).length > 0){
+      if (this.props.users && Object.values(this.props.users).length > 0 && this.props.users[this.props.lesson.authorId]){
         return (
           <div className="lesson-show-wrap">
             <LeftSidebar />
